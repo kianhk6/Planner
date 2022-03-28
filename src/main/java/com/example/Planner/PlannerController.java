25dcc6bfe45631e8c5c432a994a291afd08a6b8f
Kian <kianhk6@gmail.com>
2022-03-28 14:52:37 -0700

    Initial commit

---
+++ src/main/java/com/example/Planner/PlannerController.java
@@ -0,0 +1,18 @@
+package com.example.Planner;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PlannerController {
+    public final int MAX_CORNER = 9;
+    public final int MIN_CORNER = 0;
+
+    @GetMapping("/about")
+    public String getMyName(){
+        return "Kian Hosseinkhani";
+    }
+
+}
