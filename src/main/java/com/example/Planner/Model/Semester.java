7f0595f08bb5b395c28e0ec2342c297459426082
Kian <kianhk6@gmail.com>
2022-03-28 15:11:18 -0700

    some classes added

---
+++ src/main/java/com/example/Planner/Model/Semester.java
@@ -0,0 +1,12 @@
+package com.example.Planner.Model;
+
+public class Semester {
+    int year;
+    int term;
+
+    public Semester(int year, int term) {
+        this.year = year;
+        this.term = term;
+    }
+
+}
